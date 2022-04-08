95715eda59cd987dbcb447e8e634e1efc10964b8
andrey <Marazmatik2000@Yandex.ru>
2022-04-09 02:43:52 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# scrapy_parser_pep
-Parser PEP
+# Scrappy_parser_pep
+
+Парсер страницы https://peps.python.org/  который собирает в файл список PEP, 
+и в отдельный файл расчитывает и сохраняет количество PEP находящихся в каждом
+статусе
+
+## Подготовка и запуск проекта
+
+Склонировать репозиторий на локальную машину:
+```
+git clone https://github.com/Andrey-oss-ai/scrapy_parser_pep.git
+```
+Установить и активировать виртуальное окружение
+```
+python -m venv env
+source env/bin/activate         # для MacOS
+source env/scripts/activate     # для Windows
+```
+Установить требуемые зависимости
+```
+pip install -r requirements.txt
+```
+Далее запуск скрипта командой
+```
+scrapy crawl pep
+```
+## Результат работы скрипта
+
+В ходе работы скрипта в папке results будет создано 2 файла
+
+- pep - куда будет собраны все PEP в виде csv файла в формате <номер,имя,статус>
+
+- status summary - куда будет сгенерирована таблица с количеством PEP находящимся
+в каждом статусе
+
+## Технологии
+В ходе разработки использованы
+
+- Scrappy
+## Автор
+
+andreu-antonov@yandex.ru
---
+++ pep_parse/pipelines.py
@@ -0,0 +1,33 @@
+import csv
+import datetime as dt
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class PepParsePipeline:
+    def open_spider(self, spider):
+        self.statuses = {}
+        self.count = 0
+
+    def process_item(self, item, spider):
+        status = item['status']
+        self.statuses[status] = self.statuses.get(status, 0) + 1
+        self.count += 1
+        return item
+
+    def close_spider(self, spider):
+        results_dir = BASE_DIR / 'results'
+        results_dir.mkdir(exist_ok=True)
+        now = dt.datetime.now()
+        now_formatted = now.strftime('%Y-%m-%d_%H-%M-%S')
+        file_name = f'status_summary_{now_formatted}.csv'
+        file_path = results_dir / file_name
+        header = ['Статус', 'Количество']
+        with open(file_path, 'w', encoding='utf-8', newline='') as f:
+            writer = csv.writer(f, dialect='unix', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(header)
+            for key, value in self.statuses.items():
+                data = [str(key), str(value)]
+                writer.writerow(data)
+            writer.writerow(['Total', self.count])
